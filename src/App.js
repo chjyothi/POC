09771cfe13f50f555aea5ae1dd3085546fb40000
chjyothi <chjyothireddy97@gmail.com>
2021-06-22 16:36:12 -0500

    infinite scroll added

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Posts from './Components/Posts';
+
+
+function App() {
+  return (
+    <div className="App">
+      <p>this is APP.js page</p>
+      <Posts></Posts>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Infinitescroll.js
@@ -0,0 +1,7 @@
+import React from "react";
+class Infinitescroll extends React.Component {
+    render() {
+      return <div />;
+    }
+  }
+ export default Infinitescroll;
